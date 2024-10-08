3094a93021dbf9a6a8543855d0655fa64c95900f
sazib <sazibislam1@gmail.com>
2024-10-08 09:11:15 +0600

    init project

---
+++ site/src/jsMain/kotlin/com/blog/example/pages/models/util/Constants.kt
@@ -0,0 +1,13 @@
+package com.blog.example.pages.models.util
+
+object Constants {
+
+    const val FONT_FAMILY = "Roboto"
+}
+
+object Res {
+    object Image {
+        const val logo = "/logo.svg"
+    }
+
+}
